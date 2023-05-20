a54e24dadda2c8bea9559c3f824802196e75629b
Shashikant <shashikantprajapti17@gmail.com>
2023-05-20 12:48:58 +0530

    ...

--- js/index.js
+++ js/index.js
@@ -1,28 +1,61 @@
 const submit = document.getElementsByClassName("contact-form")[0];
+const downloadBrowser = document.getElementById("downloadBrowser");
 console.log(submit);
 const baseUrl = "";
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbxk9vKyN1F2XAduvSSWIfbnjVaHegU0VpnIE7k-GZvdpWP0qiGBCTCX7nMEXhIMSglq/exec";
 
 submit.addEventListener("submit", async (e) => {
   e.preventDefault();
-
-  const fname = document.getElementById("name3").value;
-  const email = document.getElementById("email3").value;
-  const number = document.getElementById("number").value;
   const dropdown1 = document.getElementById("dropdown1");
   var value = dropdown1.options[dropdown1.selectedIndex].value;
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycbxSujOUV0SaSWELBMY0vMrUod8_2FDuJvf_u5cuLn3erto1iLVggOJ14IwPipQDfzeZ/exec";
 
   const res = await fetch(scriptURL, {
     method: "POST",
     body: new FormData(submit),
   })
-    .then((response) =>
-      alert("Thanks for Contacting us..! We Will Contact You Soon...")
+    .then(
+      (response) =>
+        (window.location.href = `${window.location.origin}/thanks.html`)
     )
     .catch((error) => console.error("Error!", error.message));
 
-  console.log(res);
+  // console.log(res);
+
+  // Email.send({
+  //   Host: "smtp.elasticemail.com",
+  //   Username: "lalitkmr5100@gmail.com",
+  //   Password: "6B2FE6CBC47F7EE2C2836EF6385F6FC224C7",
+  //   From: "lalitkmr5100@gmail.com",
+  //   To: "shashikantprajapti17@gmail.com",
+  //   Subject: "This is the subject",
+  //   Body:
+  //     "Name: " + fname + "<br/> E-mail: " + email + "<br/> Phone: " + number + "<br/>"+ "Course: " + value,
+  // }).then((message) =>{
+  //   console.log(message)
+  // }
+  // );
+});
+
+downloadBrowser.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const dropdown1 = document.getElementById("dropdown1");
+  var value = dropdown1.options[dropdown1.selectedIndex].value;
+
+  const scriptUrl =
+    "https://script.google.com/macros/s/AKfycbxF0fChBpHfpSNDqiHxhPPHirRRzh11TNrK8_MVldBDhHi0FnhmUYP95tqqlH0mmCfqxQ/exec";
+
+  const res = await fetch(scriptUrl, {
+    method: "POST",
+    body: new FormData(downloadBrowser),
+  })
+    .then(
+      (response) =>
+        (window.location.href = `${window.location.origin}/Weeb Academy.pdf`)
+    )
+    .catch((error) => console.error("Error!", error.message));
+
+  // console.log(res);
 
   // Email.send({
   //   Host: "smtp.elasticemail.com",
